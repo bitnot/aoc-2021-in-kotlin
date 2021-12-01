406ddadae2e694159af553542208f21295c66a34
Pavel <466069+bitnot@users.noreply.github.com>
2021-12-01 08:06:46 +0100

    Day 1, puzzle 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,25 @@
 fun main() {
+    fun countIncreasingDepths(depths: List<Long>) = depths
+        .zip(depths.drop(1))
+        .fold(0) { acc, t ->
+            acc + if (t.first < t.second) 1 else 0
+        }
+
     fun part1(input: List<String>): Int {
         val depths = input.map { it.toLong() }
-        return depths
-            .zip(depths.drop(1))
-            .fold(0) { acc, t ->
-                acc + if (t.first < t.second) 1 else 0
-            }
+        return countIncreasingDepths(depths)
     }
 
-    // fun part2(input: List<String>): Int {
-    //     return input.size
-    // }
+    fun slidingSums(nums: List<Long>, window: Int): List<Long> = nums.windowed(window).map { it.sum() }
+
+    fun part2(input: List<String>): Int {
+        val depths = input.map { it.toLong() }
+        return countIncreasingDepths(slidingSums(depths, 3))
+    }
 
     check(part1(readInput("Day01_puzzle1_input1")) == 7)
     println(part1(readInput("Day01_puzzle1_input2")))
 
-    // val input = readInput("Day01")
-    // println(part1(input))
-    // println(part2(input))
+    check(part2(readInput("Day01_puzzle1_input1")) == 5)
+    println(part2(readInput("Day01_puzzle1_input2")))
 }
