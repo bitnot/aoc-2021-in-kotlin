ea9c76167d8483fbe6e29ae0b7031567af526d94
Pavel <466069+bitnot@users.noreply.github.com>
2021-12-01 07:52:16 +0100

    Day 1, puzzle 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val depths = input.map { it.toLong() }
+        return depths
+            .zip(depths.drop(1))
+            .fold(0) { acc, t ->
+                acc + if (t.first < t.second) 1 else 0
+            }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    // fun part2(input: List<String>): Int {
+    //     return input.size
+    // }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(readInput("Day01_puzzle1_input1")) == 7)
+    println(part1(readInput("Day01_puzzle1_input2")))
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    // val input = readInput("Day01")
+    // println(part1(input))
+    // println(part2(input))
 }
