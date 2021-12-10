5b6f5a41c4a521fcea37a2ea93bd4e86cec4dd74
Pavel <466069+bitnot@users.noreply.github.com>
2021-12-10 15:35:35 +0100

    Day 4, Part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -8,14 +8,14 @@ inline fun <reified T> transpose(xs: Array<Array<T>>): Array<Array<T>> {
     }
 }
 
-class Board(private val numbers: Array<Array<Int>>) {
-    private val transposed = transpose(numbers)
-    private val unmarked = numbers.flatten().toSortedSet()
+class Board(private val rows: Array<Array<Int>>) {
+    private val columns = transpose(rows)
+    private val unmarked = rows.flatten().toSortedSet()
     fun mark(number: Int): Boolean = unmarked.remove(number)
 
     fun isBingo(): Boolean {
-        fun Array<Array<Int>>.rowBingo() = this.any { row -> row.none { num -> unmarked.contains(num) } }
-        return numbers.rowBingo() || transposed.rowBingo()
+        fun Array<Array<Int>>.bingo() = this.any { row -> row.none { num -> unmarked.contains(num) } }
+        return rows.bingo() || columns.bingo()
     }
 
     fun score(): Int = unmarked.sum()
@@ -23,7 +23,7 @@ class Board(private val numbers: Array<Array<Int>>) {
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
+    fun parseInput(input: List<String>): Pair<List<Int>, List<Board>> {
         val numbers = input.first().split(",").map { it.toInt() }
         val boards = input.drop(1)
             .windowed(6, 6)
@@ -31,12 +31,18 @@ fun main() {
                 Board(
                     sixLines.drop(1)
                         .map { line ->
-                            line.trim().split("  ", " ")
+                            line.trim()
+                                .split("  ", " ")
                                 .map { it.toInt() }
                                 .toTypedArray()
                         }.toTypedArray()
                 )
             }
+        return numbers to boards
+    }
+
+    fun part1(input: List<String>): Int {
+        val (numbers, boards) = parseInput(input)
 
         for (number in numbers) {
             for (board in boards) {
@@ -48,12 +54,30 @@ fun main() {
         return 0
     }
 
+    fun part2(input: List<String>): Int {
+        val (numbers, boards) = parseInput(input)
+
+        val remainingBoards = boards.toMutableSet()
+
+        for (number in numbers) {
+            for (board in boards) {
+                if (board.mark(number) && board.isBingo()) {
+                    remainingBoards.remove(board)
+                    if (remainingBoards.isEmpty())
+                        return board.score() * number
+                }
+            }
+        }
+
+        return 0
+    }
+
     val testInput = readTestInput(4)
     val input = readInput(4)
 
     checkEquals(part1(testInput), 4512)
     println(part1(input))
-    //
-    // checkEquals(part2(testInput), 230)
-    // println(part2(input))
+
+    checkEquals(part2(testInput), 1924)
+    println(part2(input))
 }
